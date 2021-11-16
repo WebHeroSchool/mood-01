90e63cf50f2424810bc7459c062eb99457c0a88f
Olesia <ivanova.ov_87@mail.ru>
2021-11-16 20:26:35 +0300

    modified:   src/App.js 	new file:   src/number.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { count, length } from './number';
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello Word!</p>      
+        <p>count*length={count*length}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
